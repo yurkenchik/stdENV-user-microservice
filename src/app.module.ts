c4e8db6f8dc3fbb9a3009b800bae1edb1c0f698c
yurkenchik <yura.ilchshyn@gmail.com>
2024-10-09 17:12:24 +0300

    added new dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,15 @@ import {TypeOrmModule} from "@nestjs/typeorm";
 import {dataSourceOptions} from "@studENV/shared/dist/typeorm/typeorm.config";
 import {DataSourceOptions} from "typeorm";
 import {SeedingModule} from "./seeding/seeding.module";
+import {CertificateModule} from "./certificate/certificate.module";
 
 @Module({
   imports: [
       UserModule,
       TypeOrmModule.forRoot(dataSourceOptions as DataSourceOptions),
       UserModule,
-      SeedingModule
+      SeedingModule,
+      CertificateModule
   ],
   controllers: [],
   providers: [TypeOrmModule],
